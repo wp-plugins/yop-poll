88b9624f3cfe01855c39ff2243422272ff0d5791
yourownprogrammer <yourownprogrammer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-15 17:21:24 +0000

    Fixed issues with displaying stable version
    git-svn-id: https://plugins.svn.wordpress.org/yop-poll/tags/1.0@625774 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- yop_poll.php
+++ yop_poll.php
@@ -1,7 +1,7 @@
 <?php
 	/*
 	Plugin Name: Yop Poll
-	Plugin URI: http://www.yourownprogrammer.com/wp-plugins/yop-poll/
+	Plugin URI: http://www.yourownprogrammer.com/
 	Description: Use a full option polling functionality to get the answers you need. YOP Poll is the perfect, easy to use plugin for your WordPress website.
 	Author: yourownprogrammer
 	Author URL: http://www.yourownprogrammer.com
